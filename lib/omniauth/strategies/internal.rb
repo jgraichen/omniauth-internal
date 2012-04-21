47077fe85cc4d26c4b9a7b69e7f585ee9694eb38
Jan Graichen <jan.graichen@altimos.de>
2012-04-21 15:55:50 +0200

    Remove method that are also provided from multipassword base.

--- lib/omniauth/strategies/internal.rb
+++ lib/omniauth/strategies/internal.rb
@@ -15,19 +15,6 @@ module OmniAuth
         options[:model] || ::User
       end
 
-      def username
-        return @identity.uid if @identity
-        super
-      end
-
-      def callback_phase
-        if authenticate(username, request[password_id])
-          super
-        else
-          fail!(:invalid_credentials)
-        end
-      end
-
       def authenticate(username, password)
         model.authenticate(username, password)
       end
